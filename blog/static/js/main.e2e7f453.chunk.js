c666c71fedf703ff0d914fbe2e96a61877b1e996
fer <ferconde87@gmail.com>
2021-07-12 15:09:52 +0700

    Adding a link to a 'hola mundo' react app

---
+++ blog/static/js/main.e2e7f453.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpreact-prueba"]=this["webpackJsonpreact-prueba"]||[]).push([[0],{10:function(e,t,c){"use strict";c.r(t);var n=c(1),r=c.n(n),a=c(3),s=c.n(a),i=(c(8),c(0));var o=function(){return Object(i.jsx)("div",{className:"App",children:Object(i.jsx)("h1",{children:"Hola mundo"})})};s.a.render(Object(i.jsx)(r.a.StrictMode,{children:Object(i.jsx)(o,{})}),document.getElementById("root"))},8:function(e,t,c){}},[[10,1,2]]]);
+//# sourceMappingURL=main.e2e7f453.chunk.js.map
